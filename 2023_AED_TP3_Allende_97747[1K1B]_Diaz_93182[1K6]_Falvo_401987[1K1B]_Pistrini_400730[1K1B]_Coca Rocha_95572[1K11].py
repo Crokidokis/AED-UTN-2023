f185d9058c9fbd93d4ef9767e320bb0c2e996d06
Crokidokis <cocarochagonzalo@gmail.com>
2023-09-09 19:20:40 -0300

    Commit del punto 2, tambien corregi errores de tipeo en def __init__ (estaba como _init_ y tiraba error) tambien habia error de tipeo en if __name__ (estaba como _name_ y tiraba error)

--- 2023_AED_TP3_Allende_97747[1K1B]_Diaz_93182[1K6]_Falvo_401987[1K1B]_Pistrini_400730[1K1B]_Coca Rocha_95572[1K11].py
+++ 2023_AED_TP3_Allende_97747[1K1B]_Diaz_93182[1K6]_Falvo_401987[1K1B]_Pistrini_400730[1K1B]_Coca Rocha_95572[1K11].py
@@ -1,5 +1,5 @@
 class Ticket:
-    def _init_(self, cod, pat, vehiculo, pago, pais, dist):
+    def __init__(self, cod, pat, vehiculo, pago, pais, dist):
         self.codigo = cod
         self.patente = pat
         self.vehiculo = vehiculo
@@ -10,7 +10,6 @@ class Ticket:
     def _str_(self):
         cad = 'Código:  {}  | Patente N°  {}  | Tipo de vehículo:  {}  | Forma de pago:  {}  | País de la cabina:  {}  | Distancia recorrida:  {}'
         return cad.format(self.codigo, self.patente, self.vehiculo, self.pago, self.pais, self.distancia)
-
 
 def cargar_vector():
     primera = True
@@ -30,8 +29,50 @@ def cargar_vector():
             v.append(t)
     m.close()
     return v
-    
-def ordenar_vector(v)
+
+def cargar_ticket_por_teclado(v):
+    codigo = input("Ingrese el código del ticket: ")
+    while not codigo.isdigit() or len(codigo) != 10:
+        print("El código del ticket debe tener 10 dígitos numéricos.")
+        codigo = input("Ingrese el código del ticket: ")
+
+    patente = input("Ingrese la patente del vehículo: ")
+    while len(patente) != 7:
+        print("La patente debe tener 7 carácteres.")
+        patente = input("Ingrese la patente del vehículo (7 carácteres): ")
+
+    tipo_vehiculo = input("Ingrese el tipo de vehículo (0: motocicleta, 1: automóvil, 2: camión): ")
+    while tipo_vehiculo not in ['0', '1', '2']:
+        print("Tipo de vehículo inválido, ingrese el valor correctamente.")
+        tipo_vehiculo = input("Ingrese el tipo de vehículo (0: motocicleta, 1: automóvil, 2: camión): ")
+
+    forma_pago = input("Ingrese la forma de pago (1: manual, 2: telepeaje): ")
+    while forma_pago not in ['1', '2']:
+        print("La forma de pago debe ser 1 o 2.")
+        forma_pago = input("Ingrese la forma de pago (1: manual, 2: telepeaje): ")
+
+    pais = input("Ingrese el país de la cabina (0: Argentina, 1: Bolivia, 2: Brasil, 3: Paraguay, 4: Uruguay): ")
+    while pais not in ['0', '1', '2', '3', '4']:
+        print("País incorrecto, ingrese uno válido.")
+        pais = input("Ingrese el país de la cabina (0: Argentina, 1: Bolivia, 2: Brasil, 3: Paraguay, 4: Uruguay): ")
+
+    distancia = input("Ingrese la distancia en kilómetros (debe tener 3 dígitos, 000 si es la primera cabina): ")
+    while not distancia.isdigit() or len(distancia) == 3:
+        print("La distancia debe ser un número entero de 3 dígitos.")
+        distancia = input("Ingrese la distancia en kilómetros (debe tener 3 dígitos, 000 si es la primera cabina): ")
+
+    codigo = int(codigo)
+    tipo_vehiculo = int(tipo_vehiculo)
+    forma_pago = int(forma_pago)
+    pais = int(pais)
+    distancia = int(distancia)
+
+    nuevo_ticket = Ticket(codigo, patente, tipo_vehiculo, forma_pago, pais, distancia)
+    v.append(nuevo_ticket)
+
+    print("Ticket agregado con exitosamente.")
+
+def ordenar_vector(v):
     n = len(v)
     for i in range(n - 1):
         for j in range(i + 1, n):
@@ -49,10 +90,10 @@ def buscar_patente(v):
     else:
         print("No hay datos")
 
-def mostrar_datos(v)
-ordenar_vector(v)
-for i in range(len(v)):
-    print(v[i])
+def mostrar_datos(v):
+    ordenar_vector(v)
+    for i in range(len(v)):
+        print(v[i])
 
 def principal():
     # opcion 1
@@ -63,5 +104,5 @@ def principal():
     buscar_patente(v)
 
 
-if _name_ == '__main__':
+if __name__ == '__main__':
     principal()
