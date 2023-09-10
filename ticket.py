1fcdc941c8a3aa4fd55c5e9260604dfc9e478afa
Ssanti2 <144454470+Ssanti2@users.noreply.github.com>
2023-09-10 13:01:20 -0300

    Update ticket.py

--- ticket.py
+++ ticket.py
@@ -1,11 +1,14 @@
-class Ticket: #Definición de la Clase
-    def __init__(self, cod, pat, vehiculo, pago, pais, dist):
+class Ticket: # Definición de la Clase
+    def __init__(self, cod, pat, ppat,  vehiculo, pago, pais, dist):
         self.codigo = cod
         self.patente = pat
+        self.pais_patente = ppat
         self.vehiculo = vehiculo
         self.pago = pago
         self.pais = pais
         self.distancia = dist
+
     def __str__(self):
-        cad = 'Código:  {}  | Patente N°  {}  | Tipo de vehículo:  {}  | Forma de pago:  {}  | País de la cabina:  {}  | Distancia recorrida:  {}'
-        return cad.format(self.codigo, self.patente, self.vehiculo, self.pago, self.pais, self.distancia)
+        cad = 'Código:  {}  | Patente N°  {:^7} - {:<9}  | Tipo de vehículo:  {}  | Forma de pago:  {}  |' \
+              ' País de la cabina: {}  | Distancia recorrida:  {}'
+        return cad.format(self.codigo, self.patente, self.pais_patente, self.vehiculo, self.pago, self.pais, self.distancia)
